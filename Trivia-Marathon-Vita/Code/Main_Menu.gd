89bb183691b523adc447aa13241a4bb58c1e5aae
hbeaumont2204 <harrybsmith06@yahoo.co.uk>
2026-07-01 21:59:53 +0100

    V0.1.2

--- Trivia-Marathon-Vita/Code/Main_Menu.gd
+++ Trivia-Marathon-Vita/Code/Main_Menu.gd
@@ -5,9 +5,15 @@ onready var previous_score_label = $ScoreContainer/PreviousScore
 onready var options = $OptionsContainer
 onready var credits = $CreditsContainer
 onready var game_mode_button = $OptionsContainer/GameModeButton
+onready var MT_Options = $OptionsContainer/MTContainer
 
 var high_score_path = "user://high_score.dat"
 var previous_score_path = "user://previous_score.dat"
+
+var manifest_path_mt = "res://Assets/manifest_mt.txt"
+var manifest_path_tf = "res://Assets/manifest_tf.txt"
+var files_mt = ["res://Assets/quiz_pack_2.csv", "res://Assets/quiz_pack_1.csv"]
+var files_tf = ["res://Assets/quiz_history_tf.csv", "res://Assets/tf_pack_1.csv"]
 
 var high_score
 var previous_score
@@ -23,6 +29,49 @@ func _ready():
 	
 	high_score_label.text = "High Score: %s" % high_score
 	previous_score_label.text = "Previous Score: %s" % previous_score 
+	
+	# Test
+	#write_to_manifest(["res://Assets/quiz_pack_2.csv", "res://Assets/quiz_pack_1.csv"], manifest_path_mt)
+	#write_to_manifest(["res://Assets/quiz_history_tf.csv", "res://Assets/tf_pack_1.csv"], manifest_path_tf)
+	
+	files_mt = read_manifest(manifest_path_mt)
+	files_tf = read_manifest(manifest_path_tf)
+	
+	print(files_mt)
+	print(files_tf)
+	
+	if !files_mt.has("res://Assets/Quiz Packs/lit_mt.csv"):
+		set_button_red($OptionsContainer/MTContainer/LiteratureMT)
+	if !files_mt.has("res://Assets/Quiz Packs/religion_mult_choice.csv"):
+		set_button_red($OptionsContainer/MTContainer/ReligionMT)
+	if !files_mt.has("res://Assets/Quiz Packs/video_game_mt.csv"):
+		set_button_red($OptionsContainer/MTContainer/VideoGamesMT)
+	
+	
+
+func read_manifest(var manifest_path):
+	var file = File.new()
+	var files = []
+	
+	if file.open(manifest_path,File.READ) == OK:
+		while not file.eof_reached():
+			var line = file.get_line()
+			files.append(line)
+		file.close()
+	else:
+		print("Failed to open file: ", manifest_path)
+	
+	return files
+		
+func write_to_manifest(var files, var manifest_path):
+	var file = File.new()
+	
+	if file.open(manifest_path, File.WRITE) == OK:
+		for pack in files:
+			file.store_line(pack)
+		file.close()
+	else:
+		print("Write error: ", manifest_path)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -74,11 +123,43 @@ func _on_GameModeButton_pressed():
 	if game_mode_tf:
 		game_mode_tf = false
 		game_mode_button.text = "True or False"
+		MT_Options.visible = true
 	else:
 		game_mode_tf = true
 		game_mode_button.text = "Multiple choice"
+		MT_Options.visible = false
 
 
 func _on_CreditsButton_pressed():
 	options.visible = false
 	credits.visible = !credits.visible
+
+func topic_button_press(var button, var pack):
+	if button.modulate == Color(0, 1, 0):
+		button.modulate = Color(1, 0, 0)
+		if !game_mode_tf:
+			files_mt.erase(pack)
+			write_to_manifest(files_mt,manifest_path_mt)
+		else:
+			files_tf.erase(pack)
+			write_to_manifest(files_tf,manifest_path_tf)
+	else:
+		button.modulate = Color(0, 1, 0)
+		if !game_mode_tf:
+			files_mt.append(pack)
+			write_to_manifest(files_mt,manifest_path_mt)
+		else:
+			files_tf.append(pack)
+			write_to_manifest(files_tf,manifest_path_tf)
+			
+func set_button_red(var button):
+	button.modulate = Color(1, 0, 0)
+
+func _on_VideoGamesMT_pressed():
+	topic_button_press($OptionsContainer/MTContainer/VideoGamesMT,"res://Assets/Quiz Packs/video_game_mt.csv")
+
+func _on_ReligionMT_pressed():
+	topic_button_press($OptionsContainer/MTContainer/ReligionMT,"res://Assets/Quiz Packs/religion_mult_choice.csv")
+
+func _on_LiteratureMT_pressed():
+	topic_button_press($OptionsContainer/MTContainer/LiteratureMT,"res://Assets/Quiz Packs/lit_mt.csv")
